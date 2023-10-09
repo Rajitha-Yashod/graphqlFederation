06c02ccec0733c41267de7bee916916a3c6fc7c7
Rajitha-Yashod <rajithayashod@gmail.com>
2023-10-09 17:56:53 +0530

    GraphQl filtering

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 const {ApolloServer, gql} = require('apollo-server');
-const empoyees=require('./Data/employees')
+const EmployeeService =require('./dataSources/file')
 
 const typeDefs = gql`
 type Query{
-    empoyees: [Employee]
-}
+    employees(
+        id:ID,
+    firstName:String,
+    lastName:String,
+    designation:String,
+    department: String,
+    nearestCity: String
+    ): [Employee]
+
+    findEmployeeById(id:ID):Employee
+    }
 
 type Employee{
     id:ID!,
@@ -14,17 +23,26 @@ type Employee{
     department: String,
     nearestCity: String
 }
+
 `
 const resolvers = {
     Query: {
-        empoyees:()=>{
-            return empoyees
+        employees:(parent, args, {dataSources}, into)=>{
+            return dataSources.employeesService.getEmployee(args)
+        },
+        findEmployeeById(parent, {id}, {dataSources}, into){
+            return dataSources.employeesService.getEmployeeById(id)[0]
         }
+        
     }
 }
 
+const dataSources=()=>({
+     employeesService: new EmployeeService()
+})
 
-const gqlServer = new ApolloServer({typeDefs, resolvers});
+
+const gqlServer = new ApolloServer({typeDefs, resolvers, dataSources});
 
 gqlServer.listen({port:process.env.port||4000})
 .then(({url})=>console.log(`server started on ${url}`))
